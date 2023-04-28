e53632524173664f56f25a9ed928ded28d277177
Pastafredda <justspotify09@gmail.com>
2023-04-28 17:47:02 +0200

    milestone 1

--- script.js
+++ script.js
@@ -1,49 +1,55 @@
 // Dichiarazione di variabili
 let prezzoBiglietto, sconto, prezzoFinale;
 const cifra= "€";
-const chilometri= document.getElementById("chilometri");
-const userEta = document.getElementById("userEta");
+
 const salva= document.getElementById("salva");
 
 // Chiediamo quanti chilometri deve percorrere 
-document.getElementById("chilometri").value="";
-console.log(chilometri);
+
+
+salva.addEventListener("click",calcola)
+    
+function calcola(){
+    const chilometri= parseInt(document.getElementById("chilometri").value);
+    console.log(chilometri);
 
 // Chiediamo l'età
-document.getElementById("userEta").value="";
-console.log(userEta);
+    const userEta = document.getElementById("userEta").value;
+    console.log(userEta);
 
-salva.addEventListener("click",
+
+    if (userEta <= 18) {
+        // Sconto per minorenni
+        sconto = 20;
+        
+    } else if (userEta >= 65) {
+        // Sconto per over 65
+        sconto = 40;
+        
+    } else{
+       sconto=0
+    }
     
-)
-
-
-
-
-if (userEta <= 18) {
-    // Sconto per minorenni
-    sconto = 20;
+    console.log(sconto);
     
-} else if (userEta >= 65) {
-    // Sconto per over 65
-    sconto = 40;
+    // Prezzo del biglietto 
+    prezzoBiglietto = chilometri * 0.21 ;
+    console.log(prezzoBiglietto);
     
-} else{
-   sconto=0
+    // Sconto
+    bigliettoSconto = prezzoBiglietto * sconto / 100  ;
+    console.log(bigliettoSconto);
+    
+    prezzoFinale = prezzoBiglietto - bigliettoSconto ;
+    console.log(prezzoFinale);
+    
+    
+    document.getElementById("costo").innerHTML= prezzoFinale.toFixed(2) + cifra;
 }
 
-console.log(sconto);
-
-// Prezzo del biglietto 
 
 
-// Sconto
-bigliettoSconto = prezzoBiglietto * sconto / 100  ;
-console.log(bigliettoSconto);
-
-prezzoFinale = prezzoBiglietto - bigliettoSconto ;
-console.log(prezzoFinale);
 
 
-document.getElementById("costo").innerHTML= prezzoFinale.toFixed(2) + cifra;
+
 
